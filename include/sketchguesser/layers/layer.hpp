dbafed029c03471099d70533bb8bba3ed6b1042d
samir neupane <sneupane746@gmail.com>
2026-07-08 21:04:36 +0545

    layers.hpp added

---
+++ include/sketchguesser/layers/layer.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../tensor.hpp"
+
+class Layer
+{
+public:
+    virtual Tensor forward(const Tensor& input) = 0;
+    virtual Tensor backward(const Tensor& gradient) = 0;
+
+    virtual ~Layer() = default;
+};
